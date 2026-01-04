04a1d3172ba284b0a55dc04fb14d0563bc327518
Padmanava03 <Padmanava03@users.noreply.github.com>
2026-01-04 20:32:23 +0530

    Started building logic

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+def betterCounter(FILE_NAME):
+    d = {}
+    with open(FILE_NAME, "r") as f:
+        for word in f.readlines():
+            if not d.get(word, None):
+                d[word] = 1
+                continue
+
+            d[word] += 1
+    
+    print(d)
+
 def main():
-    print("Hello from betterfilecounter!")
+    betterCounter(f"{__file__}")
 
 
 if __name__ == "__main__":
